1f8633c56d50fffa711c1d755c6b1f43c5ebb5ab
Marco Antonio C <designsma.contact@gmail.com>
2026-03-01 23:20:34 -0600

    Alarma por lectura de AND

--- Alarm/Alarm.ino
+++ Alarm/Alarm.ino
@@ -1,6 +1,13 @@
 #define enA 11 // Pin de control de velocidad (PWM)
 #define in1 13 // Pin de control de dirección 1
 #define in2 12 // Pin de control de dirección 2
+// --------- Salidas para operadores AND ------
+#define logic1 2 
+#define logic2 3
+#define logic3 4
+// --------- Entradas para operadores AND ------
+#define input 5
+
 
 //---------------BOCINA-------------------
 #include <avr/pgmspace.h>
@@ -12,14 +19,13 @@ const int sampleRate = 8000; // Frecuencia de muestreo en Hz
 #define speakerPin 9 // Pin de salida para el altavoz (PWM)
 
 volatile int sampleIndex = 0;
-int repeticiones = 4;      // cuántas veces quieres reproducir
-int conteoReps = 0;
 
 unsigned long ultimoSample = 0;
-unsigned long samplePeriod = 1000000UL / sampleRate; // microsegundos por muestra (duracion de sonido)
+unsigned long samplePeriod = 1000000UL / sampleRate; // microsegundos por muestra
 //----------------------------------------
 
 int estado = 0;   // 0 = espera, 1 = rampa
+volatile bool flag = false;
 int pwmValor = 0; // Variable para almacenar el valor PWM
 unsigned long tiempoInicio = 0; // Variable para almacenar el tiempo de inicio
 unsigned long intervalo = 1000000; // 1 segundo en microsegundos
@@ -30,38 +36,52 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(speakerPin, OUTPUT);
 
+  // Configura las salidas lógicas para operadores AND
+  pinMode(logic1, OUTPUT);
+  pinMode(logic2, OUTPUT);
+  pinMode(logic3, OUTPUT);
+
+  // Configura la entrada para operadores AND
+  pinMode(input, INPUT);
+
   // Rotar el motor en una dirección
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
+
+  // Salidas and
+  digitalWrite(logic1, HIGH);
+  digitalWrite(logic2, HIGH);
+  digitalWrite(logic3, HIGH);
 
   // Configura Timer1 para reproducir audio a 8-bit Fast PWM
   TCCR1A = _BV(COM1A1) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS10);  // Sin prescaler (máxima velocidad)
   OCR1A = 128; // Nivel medio (silencio)
+  
+
 }
 
 void loop() {
 
   unsigned long ahora = micros();
+  
+  estado = digitalRead(input);
 
   switch (estado) {
 
-    case 0:
-      // Espera 1 segundo antes de iniciar
-      if (ahora - tiempoInicio >= intervalo) {
-        tiempoInicio = ahora;
-        estado = 1;
-        sampleIndex = 0;
-        conteoReps = 0;
-      }
+    case LOW: // La lectura es cero, motor apagado
+      analogWrite(enA, 0); // Motor apagado
+      OCR1A = 128; // Silencio en el altavoz
+      ultimoSample = ahora; // Reiniciar temporizador de audio
+      sampleIndex = 0; // Reiniciar índice de muestra
       break;
 
-    case 1:
+    case HIGH:
 
       // Motor a velocidad fija 50
       analogWrite(enA, 50);
 
-      // Control de muestreo a 8000 Hz
+      // si ha pasado el tiempo para la siguiente muestra, reproducir, sino esperar
       if (ahora - ultimoSample >= samplePeriod) {
 
         ultimoSample += samplePeriod;
@@ -75,16 +95,7 @@ void loop() {
 
         // Si terminó la muestra
         if (sampleIndex >= sounddata_length) {
-          sampleIndex = 0;
-          conteoReps++;
-
-          if (conteoReps >= repeticiones) {
-            // Apagar todo
-            OCR1A = 128;       // silencio
-            analogWrite(enA, 0);
-            estado = 0;
-            tiempoInicio = ahora;
-          }
+          sampleIndex = 0; // Reiniciar índice para repetir
         }
       }
 
